654c41645c4e6882fbfd34dac3442d05fc5b9bad
iqra271 <iqrashaheen271@gmail.com>
2024-04-02 19:14:29 +0500

    cookies_policy change

--- components/CookiesModal.tsx
+++ components/CookiesModal.tsx
@@ -16,18 +16,20 @@ export default function CookiesModal({
           <p className="mb-4">
             This website uses cookies to help you have a superior and more
             admissible browsing experience on the website.{" "}
-            <Link href="/cookies-policy">Read More.</Link>
+            <Link href="/cookies-policy" className="font-bold">
+              Read More.
+            </Link>
           </p>
           <button
             id="cookie-accept-button"
-            className="w-28 rounded-lg bg-gradient-to-r from-green-600 to-green-400 px-6 py-2 text-blue-100"
+            className="w-28 rounded-lg bg-gradient-to-t from-green-600 to-green-400 px-6 py-2 text-white"
             onClick={closeModal}
           >
             Accept
           </button>
           <button
             id="cookie-decline-button"
-            className="text-md text-gradient ml-2 w-28 rounded-lg border border-green-600 px-7 py-2 font-semibold text-cyan-300"
+            className="text-md text-gradient ml-2 w-28 rounded-lg border border-green-600 px-7 py-2 font-regular text-green-600"
             onClick={closeModal}
           >
             Decline
